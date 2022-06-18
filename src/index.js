b9938be3da5577e9d7bed3c0ac95a1c8b530e27b
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-06-18 03:09:27 +0100

    upgraded dependencies versions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import { P, Img, H1, A } from "@riadh-adrani/recursive-components/Standard.js";
-import { Column } from "@riadh-adrani/recursive-components/Utility.js";
-import { setStyle, DevMode, Render } from "@riadh-adrani/recursive";
+import { Column, Spacer } from "@riadh-adrani/recursive-components/Utility.js";
+import { setStaticStyle, Render } from "@riadh-adrani/recursive";
 
 import logo from "./logo.png";
 
-// Enable disable dev logs
-DevMode(false);
-
 // Create a style Sheet
-setStyle({
+setStaticStyle({
     selectors: {
         "body,html": {
             margin: "0px",
@@ -29,6 +26,16 @@ setStyle({
             margin: "0px",
             padding: "5px",
         },
+        ".used": {
+            padding: "8px",
+            marginBottom: "10px",
+            fontStyle: "italic",
+            fontFamily: "monospace",
+            fontSize: "17px",
+            color: "white",
+            backgroundColor: "#cf2026",
+            borderRadius: "4px",
+        },
     },
     animations: {
         float: {
@@ -48,7 +55,7 @@ Render(() =>
         style: {
             className: "wrapper",
             normal: {
-                border: "2px solid #cf202655",
+                border: "2px solid #cf2026",
                 borderRadius: "5px",
                 padding: "30px 70px",
                 alignItems: "center",
@@ -66,31 +73,34 @@ Render(() =>
                     normal: { animation: "float 1.5s ease-out infinite" },
                 },
             }),
-            H1({ children: "Hello Recrusive" }),
-            P({
-                children: "Thank you for trying Recursive 😀",
-                style: {
-                    className: "hello-recursive",
-                    normal: {
-                        padding: "5px",
-                        margin: "0px",
-                    },
-                },
-            }),
+            H1({ children: "Create Recursive App" }),
+            Spacer({ height: "30px" }),
             A({
                 href: "https://github.com/RiadhAdrani/recursive",
                 target: "blank",
-                style: {
-                    className: "recursive-link",
-                    scoped: true,
-                    normal: {
-                        background: "#cf202655",
-                        marginTop: "15px",
-                        padding: "5px 10px",
-                        color: "white",
-                    },
-                },
-                children: ["Support the project on github"],
+                props: { className: "used" },
+                children: "@riadh-adrani/recursive : 0.7.12",
+            }),
+            A({
+                href: "https://github.com/RiadhAdrani/create-recursive-app",
+                target: "blank",
+                props: { className: "used" },
+                children: "@riadh-adrani/create-recursive-app : 1.0.9",
+            }),
+            A({
+                href: "https://github.com/RiadhAdrani/recursive-components",
+                target: "blank",
+                props: { className: "used" },
+                children: "@riadh-adrani/recursive-components : 0.0.4",
+            }),
+            Spacer({ height: "30px" }),
+            P({ children: "Thank you for trying Recursive 😀" }),
+            Spacer({ height: "10px" }),
+            A({
+                href: "https://github.com/RiadhAdrani/recursive",
+                target: "blank",
+                props: { className: "used" },
+                children: "Support the project on github",
             }),
         ],
     })
